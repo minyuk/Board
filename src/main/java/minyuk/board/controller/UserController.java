618fa33c700e773202da98c537f76267b6c61ecc
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-05-06 23:03:35 +0900

    UserValidation

--- src/main/java/minyuk/board/controller/UserController.java
+++ src/main/java/minyuk/board/controller/UserController.java
@@ -1,14 +1,19 @@
 package minyuk.board.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import minyuk.board.controller.form.UserForm;
 import minyuk.board.domain.User;
 import minyuk.board.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/user")
@@ -22,7 +27,19 @@ public class UserController {
     }
 
     @PostMapping("/new")
-    public String create(UserForm form) {
+    public String create(@Validated UserForm form, BindingResult bindingResult) {
+
+        if (form.getPassword() != null && form.getPasswordCheck() != null) {
+            if (!form.getPassword().equals(form.getPasswordCheck())) {
+                bindingResult.reject("passwordMismatch", null);
+            }
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors= {}", bindingResult);
+            return "user/joinForm";
+        }
+
         User user = new User();
         user.setLoginId(form.getLoginId());
         user.setPassword(form.getPassword());
