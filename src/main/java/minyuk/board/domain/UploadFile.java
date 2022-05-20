ea5d4f49b39b18cc17f9323867dd582e40551c29
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-05-20 22:32:01 +0900

    File

---
+++ src/main/java/minyuk/board/domain/UploadFile.java
@@ -0,0 +1,30 @@
+package minyuk.board.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter @Setter
+public class UploadFile {
+
+    @Id @GeneratedValue
+    @Column(name = "file_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    private String uploadFileName;
+    private String storeFileName;
+
+    public UploadFile(String uploadFileName, String storeFileName) {
+        this.uploadFileName = uploadFileName;
+        this.storeFileName = storeFileName;
+    }
+}
+
